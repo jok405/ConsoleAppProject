0eb5aa941d8f6b493c68c28344c8d1e8a4a9b7d7
jok405 <john.y.kim84@gmail.com>
2018-08-14 09:43:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppProject
+{
+    class MainClass
+    {
+        interface IItem
+        {
+            string name { get; set; }
+            int goldValue { get; set; }
+
+            void Equip();
+            void Sell();
+        }
+
+        class Sword : IItem
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+
+            public Sword (string _name)
+            {
+                name = _name;
+                goldValue = 100;
+            }
+            public void Equip ()
+            {
+                Console.WriteLine(name + " equipped.");
+            }
+            public void Sell ()
+            {
+                Console.WriteLine(name + " sold for " + goldValue + " imaginary dollars!");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Sword sword = new Sword("Sword of Destiny");
+            sword.Equip();
+            sword.Sell();
+            Console.ReadKey();
+        }
+    }
+}
